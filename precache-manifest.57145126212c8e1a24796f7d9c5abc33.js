617cbbef0ddc30980f6052bc500d8fcbd2dc21b2
Kajonczyk <kopczackikamil2000@gmail.com>
2020-04-23 14:12:16 +0200

    Updates

---
+++ precache-manifest.57145126212c8e1a24796f7d9c5abc33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3270c846f23317d5e30a6e1c047cfc",
+    "url": "/VideoApp/index.html"
+  },
+  {
+    "revision": "02575d207cfccbbdd47a",
+    "url": "/VideoApp/static/js/2.34063505.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/VideoApp/static/js/2.34063505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1a4a45cbcee859cf21",
+    "url": "/VideoApp/static/js/main.e9b6542a.chunk.js"
+  },
+  {
+    "revision": "937c816264900c615733",
+    "url": "/VideoApp/static/js/runtime-main.01b61782.js"
+  }
+]);
