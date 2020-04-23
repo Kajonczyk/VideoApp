0809f60e67ffbeabb3a8ada68cb61f9f73d2d1c4
Kajonczyk <kopczackikamil2000@gmail.com>
2020-04-23 15:03:23 +0200

    Updates

---
+++ precache-manifest.1d326837a353dddd8156eed2505dfcc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67362e7cf9bef3d71d684893e1314102",
+    "url": "/VideoApp/index.html"
+  },
+  {
+    "revision": "4f62c4e7def850b39a3a",
+    "url": "/VideoApp/static/js/2.a8ce7ec6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/VideoApp/static/js/2.a8ce7ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c16a568da589dee08b6",
+    "url": "/VideoApp/static/js/main.ce473816.chunk.js"
+  },
+  {
+    "revision": "937c816264900c615733",
+    "url": "/VideoApp/static/js/runtime-main.01b61782.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VideoApp/precache-manifest.4d19ba76b0092dc8964fabf9179e95e7.js"
+  "/VideoApp/precache-manifest.1d326837a353dddd8156eed2505dfcc6.js"
 );
 
 self.addEventListener('message', (event) => {
